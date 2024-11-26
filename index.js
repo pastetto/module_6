a44dc608919cf6cb78ca0a9bfb3120fd72b86f58
pastetto <163351890+pastetto@users.noreply.github.com>
2024-11-27 00:17:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 
 
-console.log(arrValues);
-
-// let word = prompt ('Введите строку');
-// let wordLen = word.length - 1;
-
-// for (let i=0; i < wordLen / 2; i=i+1) {
-//     if (word [i] == word [wordLen-i]) {
-//         alert ('Введенная строка - палиндром!');
-//     } else {
-//         alert ('Введенная строка - не палиндром!');
-//         break;
-//     }
-// }
-
-
 // Задание 1:
 
 // let number = prompt(`Введите любое число`);
